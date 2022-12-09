8c70be4eba1bce1048130c534c18070e4c06fb2f
Gareth Hudson <garethhudson07@gmail.com>
2022-12-09 11:25:19 +0000

    Return a promise from promisable hooks

--- src/provider.js
+++ src/provider.js
@@ -4,6 +4,10 @@ class Provider {
         boot: []
     };
 
+    #promisableHooks = [
+        'boot',
+    ];
+
     register(callback) {
         this.#hooks.register.push(callback);
 
@@ -17,11 +21,25 @@ class Provider {
     }
 
     execute(hook, container) {
+        if (this.#promisableHooks.indexOf(hook) !== -1) {
+            return this.#executePromisableHook(hook, container)
+        }
+
+        return this.#executeHook(hook, container);
+    }
+
+    #executeHook(hook, container) {
         this.#hooks[hook].forEach(callback => {
             callback(container);
         });
 
         return this;
+    }
+
+    #executePromisableHook(hook, container) {
+        return Promise.all(this.#hooks[hook].map(callback => {
+            return callback(container);
+        }));
     }
 }
 
